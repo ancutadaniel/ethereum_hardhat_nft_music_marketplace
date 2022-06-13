8214a3769163f51d3e2b48be20319ec720f54e59
ancutadaniel <ancutadaniel@gmail.com>
2022-06-13 13:01:03 +0300

    nft music

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+
+// console.log(process.env);
+
+module.exports = {
+  solidity: '0.8.4',
+  networks: {
+    // ropsten: {
+    //   url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+    //   accounts: [`${process.env.ROPSTEN_PRIVATE_KEY}`],
+    // },
+  },
+  paths: {
+    artifacts: './src/backend/artifacts',
+    sources: './src/backend/contracts',
+    cache: './src/backend/cache',
+    tests: './src/backend/test',
+  },
+};
