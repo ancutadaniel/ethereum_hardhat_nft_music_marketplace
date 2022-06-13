e86a504437dd6faf969d5b0bd9c7957ff4efb751
ancutadaniel <ancutadaniel@gmail.com>
2022-06-13 13:57:11 +0300

    deploy ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,10 +6,10 @@ require('dotenv').config();
 module.exports = {
   solidity: '0.8.4',
   networks: {
-    // ropsten: {
-    //   url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-    //   accounts: [`${process.env.ROPSTEN_PRIVATE_KEY}`],
-    // },
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.ROPSTEN_PRIVATE_KEY}`],
+    },
   },
   paths: {
     artifacts: './src/backend/artifacts',
